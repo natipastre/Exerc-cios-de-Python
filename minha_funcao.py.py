c7e601a313a8391c4d7c3e68bb666432628ce798
Natália Baptista Pastre <113951260+natipastre@users.noreply.github.com>
2025-03-08 14:53:34 -0300

    Add files via upload

---
+++ minha_funcao.py.py
@@ -0,0 +1,9 @@
+
+def quadrado(num):
+    return num ** 2
+
+numero = int(input("Digite um número:"))
+
+#Testando a função
+resultado = quadrado(numero)
+print(f"O quadrado do número passado por parâmetro é{resultado}")
